ae98a2fbac8be6e57fc8b6c9063250286e2e5bd0
Jeny Sadadia <jeny.sadadia@collabora.com>
2022-04-29 12:19:42 +0530

    src/logger.py: Refactor logger class
    Modify access control of class variable 'logger'.
    Make it private as we won't need to access it
    from outside of the class.
    Add 'config_path' for logger config file to
    constructor.
    
    Signed-off-by: Jeny Sadadia <jeny.sadadia@collabora.com>

--- src/logger.py
+++ src/logger.py
@@ -14,19 +14,16 @@ import logging.config
 class Logger:
     """Logging utility class
 
-    This class accepts `name` of the logger in its contructor, by default
-    `root` will be used if not provided.
+    This class accepts name and path of the configuration file of the logger
+    in its contructor, by default `root` will be used as name of the logger
+    if not provided.
     """
 
-    def __init__(self, name="root"):
-        self.logger = self.get_logger(name)
-
-    def get_logger(self, name):
+    def __init__(self, config_path, name='root'):
         """Returns logger object using configurations and logger name"""
-        logging.config.fileConfig('/home/kernelci/config/logger.conf')
-        self.logger = logging.getLogger(name)
-        return self.logger
+        logging.config.fileConfig(config_path)
+        self._logger = logging.getLogger(name)
 
     def log_message(self, log_level, msg):
         """Creates a log record with provided log level and message"""
-        self.logger.log(log_level, msg)
+        self._logger.log(log_level, msg)
