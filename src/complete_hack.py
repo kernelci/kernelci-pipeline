d59f307d7b7c0e153ef9548f158d4ee3bb49bdb9
Guillaume Tucker <guillaume.tucker@collabora.com>
2022-07-29 11:40:06 +0200

    complete_hack.py: add stop-gap service to complete tarball nodes
    Add a temporary service to move the tarball nodes to the "completed"
    status until proper support has been implemented for dealing with
    timeouts, checking the status of all child nodes to complete the
    parent node etc.  This enables email reports to be sent, when the
    tarball node is set as complete.
    
    Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

---
+++ src/complete_hack.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+#
+# SPDX-License-Identifier: LGPL-2.1-or-later
+#
+# Copyright (C) 2022 Collabora Limited
+# Author: Guillaume Tucker <guillaume.tucker@collabora.com>
+
+from datetime import datetime, timedelta
+import logging
+import os
+import sys
+import time
+import traceback
+
+import kernelci
+import kernelci.config
+import kernelci.db
+from kernelci.cli import Args, Command, parse_opts
+
+from logger import Logger
+
+
+class CompleteHack:
+
+    def __init__(self, configs, args):
+        self._logger = Logger("config/logger.conf", "complete")
+        self._db_config = configs['db_configs'][args.db_config]
+        api_token = os.getenv('API_TOKEN')
+        self._db = kernelci.db.get_db(self._db_config, api_token)
+        self._pending_node = None
+
+    def _check_pending_node(self):
+        now = datetime.utcnow()
+        created = datetime.fromisoformat(self._pending_node['created'])
+        expires = created + timedelta(minutes=3)
+        if now > expires:
+            self._pending_node['status'] = "complete"
+            self._db.submit({'node': self._pending_node})
+            self._pending_node = None
+
+    def run(self):
+        sub_id = self._db.subscribe_node_channel({
+            'op': 'created',
+            'name': 'tarball',
+        })
+        self._logger.log_message(logging.INFO, "Listening for new tarballs")
+        self._logger.log_message(logging.INFO, "Press Ctrl-C to stop.")
+
+        try:
+            while True:
+                if not self._pending_node:
+                    self._pending_node = self._db.receive_node(sub_id)
+                else:
+                    time.sleep(5)
+                    self._check_pending_node()
+        except KeyboardInterrupt as e:
+            self._logger.log_message(logging.INFO, "Stopping.")
+        except Exception:
+            self._logger.log_message(logging.ERROR, traceback.format_exc())
+        finally:
+            self._db.unsubscribe(sub_id)
+            return True
+
+
+class cmd_run(Command):
+    help = "Hack to set tarball nodes to complete status"
+    args = [Args.db_config]
+    opt_args = [Args.verbose]
+
+    def __call__(self, configs, args):
+        CompleteHack(configs, args).run()
+        return True
+
+
+if __name__ == '__main__':
+    opts = parse_opts('complete_hack', globals())
+    configs = kernelci.config.load('config/pipeline.yaml')
+    status = opts.command(configs, opts)
+    sys.exit(0 if status is True else 1)
