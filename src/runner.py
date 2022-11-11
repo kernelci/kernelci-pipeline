9e25d570fa11aa11354b7526d296ad9d80ebf621
Guillaume Tucker <guillaume.tucker@collabora.com>
2022-11-11 14:48:27 +0100

    src/runner.py: refactor using Service class
    Refactor runner.py using the Service base class.
    
    Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

--- src/runner.py
+++ src/runner.py
@@ -10,24 +10,20 @@ import logging
 import os
 import sys
 import tempfile
-import traceback
 
 import kernelci
 import kernelci.config
-import kernelci.db
 import kernelci.lab
 from kernelci.cli import Args, Command, parse_opts
 
-from logger import Logger
+from base import Service
 
 
-class Runner:
+class Runner(Service):
 
     def __init__(self, configs, args):
-        self._logger = Logger("config/logger.conf", "runner")
-        self._db_config = configs['db_configs'][args.db_config]
-        api_token = os.getenv('API_TOKEN')
-        self._db = kernelci.db.get_db(self._db_config, api_token)
+        super().__init__(configs, args, 'runner')
+        self._db_config_yaml = self._db_config.to_yaml()
         self._plan_configs = configs['test_plans']
         self._device_configs = configs['device_types']
         runtime_config = configs['labs'][args.lab_config]
@@ -53,7 +49,7 @@ class Runner:
         self._logger.log_message(logging.INFO, f"tmp: {tmp}")
         revision = node['revision']
         params = {
-            'db_config_yaml': self._db_config.to_yaml(),
+            'db_config_yaml': self._db_config_yaml,
             'name': plan_config.name,
             'node_id': node['_id'],
             'revision': revision,
@@ -105,11 +101,18 @@ class RunnerLoop(Runner):
         self._job_tmp_dirs = job_tmp_dirs
         # ToDo: if stat != 0 then report error to API?
 
-    def loop(self):
-        sub_id = self._db.subscribe_node_channel(filters={
+    def _setup(self, args):
+        return self._db.subscribe_node_channel(filters={
             'name': 'checkout',
             'state': 'available',
         })
+
+    def _stop(self, sub_id):
+        if sub_id:
+            self._db.unsubscribe(sub_id)
+        self._cleanup_paths()
+
+    def _run(self, sub_id):
         self._logger.log_message(logging.INFO,
                                  "Listening for complete checkout events")
         self._logger.log_message(logging.INFO,
@@ -124,45 +127,20 @@ class RunnerLoop(Runner):
             )
             return False
 
-        status = True
-
-        try:
-            while True:
-                checkout_node = self._db.receive_node(sub_id)
-                job, tmp = self._schedule_test(
-                    checkout_node, self._plan, device
-                )
-                if self._runtime.config.lab_type == 'shell':
-                    self._job_tmp_dirs[job] = tmp
-                self._cleanup_paths()
-        except KeyboardInterrupt:
-            self._logger.log_message(logging.INFO, "Stopping.")
-        except Exception:
-            self._logger.log_message(logging.ERROR, traceback.format_exc())
-            status = False
-        finally:
-            self._db.unsubscribe(sub_id)
+        while True:
+            checkout_node = self._db.receive_node(sub_id)
+            job, tmp = self._schedule_test(
+                checkout_node, self._plan, device
+            )
+            if self._runtime.config.lab_type == 'shell':
+                self._job_tmp_dirs[job] = tmp
             self._cleanup_paths()
 
-        return status
+        return True
 
 
 class RunnerSingleJob(Runner):
     """Runner subclass to execute a single job"""
-
-    def _run_single_job(self, checkout_node, plan, device):
-        try:
-            job, tmp = self._schedule_test(checkout_node, plan, device)
-            if self._runtime.config.lab_type == 'shell':
-                self._logger.log_message(logging.INFO, "Waiting...")
-                job.wait()
-                self._logger.log_message(logging.INFO, "...done")
-        except KeyboardInterrupt:
-            self._logger.log_message(logging.ERROR, "Aborting.")
-        except Exception:
-            self._logger.log_message(logging.ERROR, traceback.format_exc())
-            return False
-        return True
 
     def _get_node_from_commit(self, git_commit):
         nodes = self._db.get_nodes({
@@ -170,22 +148,30 @@ class RunnerSingleJob(Runner):
         })
         return nodes[0] if nodes else None
 
-    def run(self, args):
+    def _setup(self, args):
         if args.node_id:
             checkout_node = self._db.get_node(args.node_id)
         elif args.git_commit:
             checkout_node = self._get_node_from_commit(args.git_commit)
         else:
             checkout_node = None
-
         if checkout_node is None:
             self._logger.log_message(logging.ERROR, "Node not found")
             return False
+        return {
+            'node': checkout_node,
+            'plan': self._plan_configs[args.plan],
+            'device': self._device_configs[args.target],
+        }
 
-        plan_config = self._plan_configs[args.plan]
-        device_config = self._device_configs[args.target]
-        return self._run_single_job(checkout_node, plan_config,
-                                    device_config)
+    def _run(self, ctx):
+        node, plan, device = (ctx[key] for key in ('node', 'plan', 'device'))
+        job, tmp = self._schedule_test(node, plan, device)
+        if self._runtime.config.lab_type == 'shell':
+            self._logger.log_message(logging.INFO, "Waiting...")
+            job.wait()
+            self._logger.log_message(logging.INFO, "...done")
+        return True
 
 
 class cmd_loop(Command):
@@ -194,7 +180,7 @@ class cmd_loop(Command):
     opt_args = [Args.verbose, Args.plan]
 
     def __call__(self, configs, args):
-        return RunnerLoop(configs, args).loop()
+        return RunnerLoop(configs, args).run()
 
 
 class cmd_run(Command):
