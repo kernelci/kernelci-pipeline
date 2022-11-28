725ad21816f60cdf72b2d9ebce009f1e98c731ad
Jeny Sadadia <jeny.sadadia@collabora.com>
2022-11-28 12:30:08 +0530

    send_kcidb: refactor KCIDB bridge
    Refactor `send_kcidb` service:
    - use `Service` as a base class
    - code refactoring by defining methods
    - define configurations options for KCIDB project id
    and topic name
    
    Signed-off-by: Jeny Sadadia <jeny.sadadia@collabora.com>

--- src/send_kcidb.py
+++ src/send_kcidb.py
@@ -8,126 +8,110 @@
 # Copyright (C) 2022 Collabora Limited
 # Author: Jeny Sadadia <jeny.sadadia@collabora.com>
 
+"""KCIDB bridge service"""
+
 import datetime
-import logging
-import os
 import sys
-import traceback
 
 import kernelci
 import kernelci.db
-from kernelci.config import load
+import kernelci.config
 from kernelci.cli import Args, Command, parse_opts
 from kcidb import Client
 import kcidb
 
-from logger import Logger
+from base import Service
+
+
+class KCIDBBridge(Service):
+    def __init__(self, configs, args, name):
+        super().__init__(configs, args, name)
+
+    def _setup(self, args):
+        return {
+            'client': Client(
+                project_id=args.kcidb_project_id,
+                topic_name=args.kcidb_topic_name
+            ),
+            'sub_id': self._db.subscribe_node_channel(filters={
+                'name': 'checkout',
+                'state': 'done',
+            }),
+        }
+
+    def _stop(self, context):
+        if context['sub_id']:
+            self._db.unsubscribe(context['sub_id'])
+
+    def _send_revision(self, client, revision):
+        if kcidb.io.SCHEMA.is_valid(revision):
+            return client.submit(revision)
+        self.log.error("Aborting, invalid data")
+
+    @staticmethod
+    def _set_timezone(created_timestamp):
+        created_time = datetime.datetime.fromisoformat(created_timestamp)
+        if not created_time.tzinfo:
+            tz_utc = datetime.timezone(datetime.timedelta(hours=0))
+            created_time = datetime.datetime.fromtimestamp(
+                created_time.timestamp(), tz=tz_utc)
+        return created_time.isoformat()
+
+    def _run(self, context):
+        self.log.info("Listening for events... ")
+        self.log.info("Press Ctrl-C to stop.")
+
+        while True:
+            node = self._db.receive_node(context['sub_id'])
+            self.log.info(f"Submitting node to KCIDB: {node['_id']}")
+
+            revision = {
+                'builds': [],
+                'checkouts': [
+                    {
+                        'id': f"kernelci:{node['_id']}",
+                        'origin': 'kernelci',
+                        'tree_name': node['revision']['tree'],
+                        'git_repository_url': node['revision']['url'],
+                        'git_commit_hash': node['revision']['commit'],
+                        'git_repository_branch': node['revision']['branch'],
+                        'start_time': self._set_timezone(node['created']),
+                        'patchset_hash': '',
+                        'misc': {
+                            'submitted_by': 'kernelci-pipeline'
+                        },
+                    }
+                ],
+                'tests': [],
+                'version': {
+                    'major': 4,
+                    'minor': 0
+                }
+            }
+            self._send_revision(context['client'], revision)
+        return True
 
 
 class cmd_run(Command):
     help = "Listen for events and send them to KCDIB"
-    args = [Args.db_config]
-
-    def __init__(self, sub_parser, name):
-        super().__init__(sub_parser, name)
-        self._logger = Logger("config/logger.conf", "send_kcidb")
+    args = [
+        Args.db_config,
+        {
+            'name': '--kcidb-topic-name',
+            'help': "KCIDB topic name",
+        },
+        {
+            'name': '--kcidb-project-id',
+            'help': "KCIDB project ID",
+        },
+    ]
 
     def __call__(self, configs, args):
-        db_config = configs['db_configs'][args.db_config]
-        api_token = os.getenv('API_TOKEN')
-        db = kernelci.db.get_db(db_config, api_token)
-
-        if not os.getenv('GOOGLE_APPLICATION_CREDENTIALS'):
-            self._logger.log_message(logging.ERROR,
-                                     "No GOOGLE_APPLICATION_CREDENTIALS \
-environment variable")
-            return False
-
-        topic_name = os.getenv('KCIDB_TOPIC_NAME')
-        if not topic_name:
-            self._logger.log_message(logging.ERROR, "No KCIDB_TOPIC_NAME \
-environment variable")
-            return False
-
-        project_id = os.getenv('KCIDB_PROJECT_ID')
-        if not project_id:
-            self._logger.log_message(logging.ERROR, "No KCIDB_PROJECT_ID \
-environment variable")
-            return False
-
-        client = Client(project_id=project_id, topic_name=topic_name)
-        if client is None:
-            self._logger.log_message(logging.ERROR, "Failed to create client \
-connection to KCIDB")
-            return False
-
-        sub_id = db.subscribe_node_channel(filters={
-            'name': 'checkout',
-            'state': 'done',
-        })
-        self._logger.log_message(logging.INFO, "Listening for events... ")
-        self._logger.log_message(logging.INFO, "Press Ctrl-C to stop.")
-        sys.stdout.flush()
-
-        tz_utc = datetime.timezone(datetime.timedelta(hours=0))
-
-        try:
-            while True:
-                node = db.receive_node(sub_id)
-                self._logger.log_message(logging.INFO, f"Submitting node to \
-KCIDB: {node['_id']}")
-                sys.stdout.flush()
-
-                created_time = datetime.datetime.fromisoformat(node["created"])
-                if not created_time.tzinfo:
-                    created_time = datetime.datetime.fromtimestamp(
-                        created_time.timestamp(), tz=tz_utc)
-                revision = {
-                    "builds": [],
-                    "checkouts": [{
-                        "id": f"kernelci:{node['_id']}",
-                        "origin": "kernelci",
-                        "tree_name": node["revision"]["tree"],
-                        "git_repository_url": node["revision"]["url"],
-                        "git_commit_hash": node["revision"]["commit"],
-                        "git_repository_branch": node["revision"]["branch"],
-                        "start_time": created_time.isoformat(),
-                        "patchset_hash": "",
-                    }],
-                    "tests": [],
-                    "version": {
-                        "major": 4,
-                        "minor": 0
-                    }
-                }
-                self.send_revision(client, revision)
-
-            sys.stdout.flush()
-
-        except KeyboardInterrupt:
-            self._logger.log_message(logging.INFO, "Stopping.")
-        except Exception:
-            self._logger.log_message(logging.ERROR, traceback.format_exc())
-        finally:
-            db.unsubscribe(sub_id)
-
-        sys.stdout.flush()
-
-        return True
-
-    def send_revision(self, client, revision):
-        if self.validate_revision(revision):
-            return client.submit(revision)
-        self._logger.log_message(logging.ERROR, f"Aborting, invalid data")
-        sys.stdout.flush()
-
-    @staticmethod
-    def validate_revision(revision):
-        return kcidb.io.SCHEMA.is_valid(revision)
+        return KCIDBBridge(configs, args, 'send_kcidb').run(args)
 
 
 if __name__ == '__main__':
-    opts = parse_opts('runner', globals())
+    opts = parse_opts('send_kcidb', globals())
     configs = kernelci.config.load('config/pipeline.yaml')
     status = opts.command(configs, opts)
     sys.exit(0 if status is True else 1)
