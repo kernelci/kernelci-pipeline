fd8c3f62a58aa98cd27b44597c98ec61536a58e5
Guillaume Tucker <guillaume.tucker@collabora.com>
2023-05-05 18:00:42 +0200

    lava: implement callback using runtime.lava.Callback
    Add an initial implementation to handle LAVA callbacks using the
    kernelci.runtime.lava.Callback class.
    
    This does not rely on any lab tokens and all settings are hard-coded,
    so it's not entirely ready to be hosted publicly.  It can however be
    used to verify the logic involved with parsing LAVA callback data and
    submitting the results to the API.
    
    This also doesn't real with any artifacts such as log extracted from
    the LAVA callback, which would need to be uploaded to storage.
    
    An environment file is required with KCI_API_TOKEN in order to use the
    API, like most other pipeline services.
    
    Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

--- src/lava_callback.py
+++ src/lava_callback.py
@@ -1,10 +1,42 @@
-from flask import Flask
+# SPDX-License-Identifier: LGPL-2.1-or-later
+#
+# Copyright (C) 2023 Collabora Limited
+# Author: Guillaume Tucker <guillaume.tucker@collabora.com>
+
+import os
+
+import kernelci.api.helper
+import kernelci.config
+from kernelci.runtime.lava import Callback
+
+from flask import Flask, request
+
+
+def _get_api_helper():
+    configs = kernelci.config.load('config/pipeline.yaml')
+    api_config = configs['api_configs']['staging.kernelci.org']
+    api_token = os.getenv('KCI_API_TOKEN')
+    api = kernelci.api.get_api(api_config, api_token)
+    return kernelci.api.helper.APIHelper(api)
+
+
+api_helper = _get_api_helper()
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
     return "KernelCI API & Pipeline LAVA callback handler"
+
+
+@app.post('/node/<node_id>')
+def callback(node_id):
+    data = request.get_json()
+    job_callback = Callback(data)
+    results = job_callback.get_results()
+    job_node = api_helper.api.get_node(node_id)
+    hierarchy = job_callback.get_hierarchy(results, job_node)
+    return api_helper.submit_results(hierarchy, job_node)
 
 
 if __name__ == '__main__':
