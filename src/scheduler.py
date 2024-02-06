kernelci/kernelci-pipeline

commit d916ba8ecde0b7a2f14143c8a595a061f5f2a4ae

src/scheduler: store k8s context in the job node
For the job running in k8s runtime, get k8s cluster
name that ran the job and store it in the node data field
i.e. `data.k8s_context`.

Signed-off-by: Jeny Sadadia <jeny.sadadia@collabora.com>

src/scheduler.py
@@ -105,12 +105,18 @@ class Scheduler(Service):
 
         job_id = str(runtime.get_job_id(running_job))
         node['data']['job_id'] = job_id
+
+        if platform.name == "kubernetes":
+            context = runtime.get_context()
+            node['data']['k8s_context'] = context
+
         try:
             self._api.node.update(node)
         except requests.exceptions.HTTPError as err:
             err_msg = json.loads(err.response.content).get("detail", [])
             self.log.error(err_msg)
 
+        self.log.debug(f"job node: {node}")
         self.log.info(' '.join([
             node['id'],
             runtime.config.name,

commit 94c20326220cc6bdecd8db28947cb13f89589708

src/scheduler: remove debug log
Remove debug log for printing job node.

Fixes: d916ba8 ("src/scheduler: store k8s context in the job node")
Signed-off-by: Jeny Sadadia <jeny.sadadia@collabora.com>

src/scheduler.py
@@ -116,7 +116,6 @@ class Scheduler(Service):
             err_msg = json.loads(err.response.content).get("detail", [])
             self.log.error(err_msg)
 
-        self.log.debug(f"job node: {node}")
         self.log.info(' '.join([
             node['id'],
             runtime.config.name,