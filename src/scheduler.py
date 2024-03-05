036e43bcadec186e83f7050f2476e55dcc995577
Arnaud Ferraris <arnaud.ferraris@collabora.com>
2024-03-05 17:49:09 +0100

    scheduler: apply rules before running job
    Scheduler config entries can now contain rules to ensure jobs are only
    executed when certain conditions are met. This change ensures this check
    is properly conducted before actually attempting to run a job.
    
    Signed-off-by: Arnaud Ferraris <arnaud.ferraris@collabora.com>

--- src/scheduler.py
+++ src/scheduler.py
@@ -150,9 +150,10 @@ class Scheduler(Service):
 
         while True:
             event = self._api_helper.receive_event_data(sub_id)
-            for job, runtime, platform in self._sched.get_schedule(event):
+            for job, runtime, platform, rules in self._sched.get_schedule(event):
                 input_node = self._api.node.get(event['id'])
-                self._run_job(job, runtime, platform, input_node)
+                if self._api_helper.should_create_node(rules, input_node):
+                    self._run_job(job, runtime, platform, input_node)
 
         return True
 
