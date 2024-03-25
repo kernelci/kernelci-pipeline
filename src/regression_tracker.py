e4ed7bd1d8584e0f6e77f953a050cd82c8a8bf67
Ricardo Cañuelo <ricardo.canuelo@collabora.com>
2024-03-25 15:49:07 +0100

    regression_tracker: sync to latest model changes
    To be applied after kernelci-core commit
    
    models: rename regression.data.failed_kernel_revision to kernel_revision
    
    Signed-off-by: Ricardo Cañuelo <ricardo.canuelo@collabora.com>

--- src/regression_tracker.py
+++ src/regression_tracker.py
@@ -85,7 +85,7 @@ class RegressionTracker(Service):
             'config_full': failed_node['data'].get('config_full'),
             'compiler': failed_node['data'].get('compiler'),
             'platform': failed_node['data'].get('platform'),
-            'failed_kernel_version': failed_node['data'].get('kernel_revision'),   # noqa
+            'kernel_revision': failed_node['data'].get('kernel_revision'),   # noqa
             'error_code': error['error_code'],
             'error_msg': error['error_msg'],
         }
